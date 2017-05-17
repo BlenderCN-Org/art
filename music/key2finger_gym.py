3beed36a1ded8a2e2e3d7215930e0df5763d0f5a
Eibriel <eibriel@yahoo.com.ar>
2017-05-17 15:49:50 -0300

    Cleanup

--- music/key2finger_gym.py
+++ music/key2finger_gym.py
@@ -13,97 +13,13 @@ import matplotlib.pyplot as plt
 from collections import deque
 
 
-## Environment
-
-
-def open_midi(midigram_filepath):
-    ## Midigram collumns
-    # 1: On (MIDI pulse units)
-    # 2: Off (MIDI pulse units)
-    # 3: Track number
-    # 4: Channel number
-    # 5: Midi pitch -> 60 -> c4 / 61 -> c#4
-    # 6: Midi velocity
-
-    midi_pulse_per_second  = 480.299
-    fps = 30
-    fps = 11.7 #TODO
-    margin = 1
-    keyboard_size = 88
-    max_frames = 10000
-
-    midi_pulse_per_frame = int(midi_pulse_per_second / fps)
-
-    with open (midigram_filepath, "r") as midigram_file:
-        data=midigram_file.readlines()
-
-    last_frame = 0
-    lower_pitch = None
-    midi_frames = {}
-    for data_line in data[:-1]:
-        line_regex = re.search(r'^(?P<ontime>\d+)\s(?P<offtime>\d+)\s(?P<track>\d+)\s(?P<channel>\d+)\s(?P<pitch>\d+)\s(?P<velocity>\d+)', data_line)
-        ontime = int(int(line_regex.group("ontime"))/midi_pulse_per_frame)
-        offtime = int(int(line_regex.group("offtime"))/midi_pulse_per_frame)
-        track = int(line_regex.group("track"))
-        channel = int(line_regex.group("channel"))
-        pitch = int(line_regex.group("pitch"))
-        velocity = int(line_regex.group("velocity"))
-
-        if not ontime in midi_frames:
-            midi_frames[ontime] = []
-        midi_frames[ontime].append({
-            "ontime": ontime,
-            "offtime": offtime,
-            "track": track,
-            "channel": channel,
-            "pitch": pitch,
-            "velocity": velocity,
-        })
-        if offtime > last_frame:
-            last_frame = offtime
-        if lower_pitch == None:
-            lower_pitch = pitch
-        if pitch < lower_pitch:
-            lower_pitch = pitch
-
-    music = []
-    for frame_number in range(0, last_frame+1):
-        if frame_number > max_frames:
-            break
-        notes = [0 for _ in range(0, keyboard_size)]
-        music.append(notes)
-
-    for mf in midi_frames:
-        for mf_note in midi_frames[mf]:
-            # Normalize pitch on the keyboard
-            #mf_normalized_pitch = mf_note['pitch']-lower_pitch
-            mf_normalized_pitch = mf_note['pitch'] # Disabling normalization
-            if mf_normalized_pitch >= keyboard_size:
-                print ("Skipping note, out of keyboard range")
-                continue
-            if mf_note['track'] != 1:
-                print ("Skipping note, out of track")
-                continue
-            # Fill ontime to offtime values
-            for f in range(mf_note['ontime'], mf_note['offtime']):
-                music[f][mf_normalized_pitch] = 1.0
-
-    notes = [0 for _ in range(0, keyboard_size)]
-    music = [notes for _ in range(0, margin)] + music + [notes for _ in range(0, margin)]
-    return music
-
-## Q-Learning
-
-# Create the Cart-Pole game environment
+# Create the Piano game environment
 env = gym.make('Piano-v0')
-#env.frames = open_midi('MIDI/Hummelflug.midigram')
-#env.frame_count = len(env.frames)
-
-#env = PianoEnv('CartPole-v0', open_midi('MIDI/Hummelflug.midigram'))
-
 env.reset()
 rewards = []
-if 0:
+
+# Testing the environment
+if 1:
     for _ in range(100):
         env.render()
         state, reward, done, info = env.step(env.action_space.sample()) # take a random action
@@ -113,7 +29,6 @@ if 0:
             env.reset()
 
 print(rewards[-20:])
-
 
 class QNetwork:
     def __init__(self, learning_rate=0.01, state_size=4,
@@ -165,13 +80,13 @@ class Memory():
 
 
 train_episodes = 20000         # max number of episodes to learn from
-max_steps = 500                # max steps in an episode
+max_steps = 10000              # max steps in an episode
 gamma = 0.9                    # future reward discount
 
 # Exploration parameters
-explore_start = 0.99            # exploration probability at start
-explore_stop = 0.001            # minimum exploration probability
-decay_rate = 0.000001           # exponential decay rate for exploration prob
+explore_start = 0.99           # exploration probability at start
+explore_stop = 0.001           # minimum exploration probability
+decay_rate = 0.000001          # exponential decay rate for exploration prob
 
 # Network parameters
 hidden_size = 128              # number of units in each Q-network hidden layer
@@ -188,13 +103,13 @@ tf.reset_default_graph()
 key_count = 88
 fingers_count = 10
 state_size = (key_count * 2) + (key_count * fingers_count)
-action_size = 7 * fingers_count
+action_size = (6 * fingers_count) + 1
 print ("State Size: {}, Action Size: {}".format(state_size, action_size))
 mainQN = QNetwork(name='main', hidden_size=hidden_size, learning_rate=learning_rate, state_size=state_size, action_size=action_size)
 
 # Initialize the simulation
 env.reset()
-# Take one random step to get the pole and cart moving
+# Take one random step to generate an initial state
 state, reward, done, _ = env.step(env.action_space.sample())
 
 memory = Memory(max_size=memory_size)
@@ -202,7 +117,7 @@ memory = Memory(max_size=memory_size)
 # Make a bunch of random actions and store the experiences
 for ii in range(pretrain_length):
     # Uncomment the line below to watch the simulation
-    env.render()
+    #env.render()
 
     # Make a random action
     action = env.action_space.sample()
@@ -216,7 +131,7 @@ for ii in range(pretrain_length):
 
         # Start new episode
         env.reset()
-        # Take one random step to get the pole and cart moving
+        # Take one random step to generate an initial state
         state, reward, done, _ = env.step(env.action_space.sample())
     else:
         # Add experience to memory
@@ -252,7 +167,6 @@ if do_training:
                     feed = {mainQN.inputs_: state.reshape((1, *state.shape)), mainQN.keep_prob: 1.0}
                     Qs = sess.run(mainQN.output, feed_dict=feed)
                     action = np.argmax(Qs)
-                    #print (action)
 
                 # Take action, get new state and reward
                 next_state, reward, done, _ = env.step(action)
@@ -263,13 +177,13 @@ if do_training:
                     # the episode ends so no next state
                     next_state = np.zeros(state.shape)
 
-                    if step % 100 == 0:
+                    if ep % 100 == 0:
                         saver.save(sess, "checkpoints/cartpole.ckpt")
 
-                    if step % 10 == 0:
+                    if ep % 10 == 0:
                         print('Episode: {}'.format(ep),
                               'Step: {:04d}'.format(t),
-                              'Action: {:.4f}'.format(action),
+                              #'Action: {:.4f}'.format(action),
                               #'Finger position: {:.4f}'.format(env.current_finger_position),
                               'Total reward: {:.4f}'.format(total_reward),
                               'Training loss: {:.4f}'.format(loss),
@@ -339,9 +253,9 @@ if do_training:
 
 
 test_episodes = 10
-test_max_steps = env.frame_count
+test_max_steps = 10000
 env.reset()
-env.training = False
+#env.training = False
 recorded_sessions = []
 with tf.Session() as sess:
     saver.restore(sess, tf.train.latest_checkpoint('checkpoints'))
@@ -353,13 +267,14 @@ with tf.Session() as sess:
             env.render()
 
             # Get action from Q-network
-            feed = {mainQN.inputs_: state.reshape((1, *state.shape)), mainQN.keep_prob: 0.7}
+            feed = {mainQN.inputs_: state.reshape((1, *state.shape)), mainQN.keep_prob: 1.}
             Qs = sess.run(mainQN.output, feed_dict=feed)
             action = np.argmax(Qs)
 
             # Take action, get new state and reward
             next_state, reward, done, _ = env.step(action)
-            finger_position = env.current_finger_position
+            #finger_position = env.current_finger_position
+            #finger_position = 0
 
             if done:
                 t = test_max_steps
@@ -370,7 +285,7 @@ with tf.Session() as sess:
             else:
                 state = next_state
                 # Convert to Python list for Json compatibility and append
-                recorded_frames.append([finger_position, np.asscalar(action)] + state.tolist()[env.key_count:])
+                #recorded_frames.append([finger_position, np.asscalar(action)] + state.tolist()[env.key_count:])
                 t += 1
         recorded_sessions.append(recorded_frames)
 
