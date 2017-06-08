a948cf3fda2d65f6ed1345aa0fb6343c09299762
Eibriel <eibriel@yahoo.com.ar>
2017-06-08 17:28:51 -0300

    Updates on Key2Finger

--- music/key2finger_qlearning.py
+++ music/key2finger_qlearning.py
@@ -73,9 +73,13 @@ def open_midi(midigram_filepath):
     for mf in midi_frames:
         for mf_note in midi_frames[mf]:
             # Normalize pitch on the keyboard
-            mf_normalized_pitch = mf_note['pitch']-lower_pitch
+            #mf_normalized_pitch = mf_note['pitch']-lower_pitch
+            mf_normalized_pitch = mf_note['pitch'] # Disabling normalization
             if mf_normalized_pitch >= keyboard_size:
                 print ("Skipping note, out of keyboard range")
+                continue
+            if mf_note['track'] != 1:
+                print ("Skipping note, out of track")
                 continue
             # Fill ontime to offtime values
             for f in range(mf_note['ontime'], mf_note['offtime']):
@@ -87,24 +91,42 @@ def open_midi(midigram_filepath):
 
 
 class ActionSpace():
-    def __init__(self):
-        pass
+    def __init__(self, fingers_count):
+        self.fingers_count = fingers_count
 
     def sample(self):
-        return rd.randint(0, 6)
+        return rd.randint(0, (5**self.fingers_count)-1)
 
 
 class PianoEnv:
     def __init__(self, name, midigram_filepath):
         self.env_name = name
-        self.action_space = ActionSpace()
-
         self.frames = open_midi(midigram_filepath)
 
         self.key_count = 88
+        self.fingers_count = 5
         self.frame_count = len(self.frames)
         self.training = True
+        self.failed_frames_threshold = 300
 
+        self.action_space = ActionSpace(self.fingers_count)
+
+        print ("Generating actions")
+        step_sizes = [0, 1, -1, 2, -2]
+        self.finger_actions = []
+        for f0 in step_sizes:
+            for f1 in step_sizes:
+                for f2 in step_sizes:
+                    for f3 in step_sizes:
+                        for f4 in step_sizes:
+                            self.finger_actions.append([f0, f1, f2, f3, f4])
+        #                    for f5 in step_sizes:
+        #                        for f6 in step_sizes:
+        #                            for f7 in step_sizes:
+        #                                for f8 in step_sizes:
+        #                                    for f9 in step_sizes:
+        #                                        self.finger_actions.append([f0, f1, f2, f3, f4, f5, f6, f7, f8, f9])
+        print ("Actions generated")
         self.reset()
 
 
@@ -115,15 +137,13 @@ class PianoEnv:
         else:
             self.current_frame = 0
         current_key = int(self.key_count/2)
-        max_note_val = None
         for note_pitch, note_val in enumerate(self.frames[self.current_frame]):
-            if max_note_val == None:
+            if note_val == 1.0 and current_key > note_pitch:
                 current_key = note_pitch
-                max_note_val = note_val
-            if note_val > max_note_val:
-                current_key = note_pitch
-                max_note_val = note_val
-        self.current_finger_position = current_key
+        self.current_finger_position = []
+        for n_finger in range(0, self.fingers_count):
+            self.current_finger_position.append(current_key+n_finger)
+
         self.failed_frames = 0
 
 
@@ -138,23 +158,130 @@ class PianoEnv:
     def step(self, action):
 
         step_sizes = [0, 1, -1, 2, -2, 3, -3]
+        step_sizes = [0, 1, -1, 2, -2]
+        # Hand 1 movements: 7
+        # Hand 2 movements: 7
+        # Hand 1 positions:
+        # Hand 1 positions:
+        finger_1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] # Thumb possible positions, being 4 the center (middle finger)
+        finger_2 = [0, 1, 2, 3, 4, 5] # Index possible positions, being 0 the center (middle finger)
+        finger_4 = [0, 1, 2, 3] # Ring possible positions, being 3 the center (middle finger)
+        finger_5 = [0, 1, 2, 3, 4, 5] # Pinky possible positions, being 5 the center (middle finger)
 
-        self.current_finger_position += step_sizes[action]
+        # 7*14*6*4*6 = 14,112
+        # 7*14*6*4*6 = 14,112
+        # 14.112*14.112 = 199,148,544
 
-        one_hot = [0 for _ in range(0, self.key_count)]
-        one_hot[self.current_finger_position] = 1
+        # 14*6*4*6 ^ 2 = 4,064,256
 
-        key_value = 0.0
-        if self.current_finger_position in self.frames[self.current_frame]:
-            if key_value == 1.0:
-                reward = 0.5
+        # 7*7*7*7*7 = 16,807
+
+        # 7*5*5*5*5 = 4,375*4,375 = 19,140,625
+        # 5*5*5*5*5 = 3,125*3,125 = 9,765,625
+
+
+
+        default_reward = 0.01
+        # Apply Middle finger movements
+        for n_finger in range(0, self.fingers_count):
+            self.current_finger_position[2] += self.finger_actions[action][2]
+        if self.fingers_count > 5:
+            for n_finger in range(0, self.fingers_count):
+                self.current_finger_position[7] += self.finger_actions[action][7]
+        for index, finger_steps in enumerate(self.finger_actions[action]):
+            if index == 2 or index == 7: # If Middle fingers
+                continue # movement already applyed
+            else:
+                self.current_finger_position[index] += finger_steps
+                if index == 0:
+                    # Apply constrains Left hand fingers (constrained to left Middle)
+                    if self.current_finger_position[index] < self.current_finger_position[2]-5:
+                        # If left Pinky is more than 5 keys away to the left, constrain
+                        self.current_finger_position[index] = self.current_finger_position[2]-5
+                    elif self.current_finger_position[index] > self.current_finger_position[2]:
+                        # If left Pinky is more than 0 keys away to the right, constrain
+                        self.current_finger_position[index] = self.current_finger_position[2]
+                elif index == 1:
+                    if self.current_finger_position[index] < self.current_finger_position[2]-3:
+                        # If left Ring is more than 3 keys away to the left, constrain
+                        self.current_finger_position[index] = self.current_finger_position[2]-3
+                    elif self.current_finger_position[index] > self.current_finger_position[2]:
+                        # If left Ring is more than 0 keys away to the right, constrain
+                        self.current_finger_position[index] = self.current_finger_position[2]
+                elif index == 3: # We skip finger 2, since it is the Left Middle finger
+                    if self.current_finger_position[index] > self.current_finger_position[2]+5:
+                        # If left Index is more than 5 keys away to the right, constrain
+                        self.current_finger_position[index] = self.current_finger_position[2]+5
+                    elif self.current_finger_position[index] < self.current_finger_position[2]:
+                        # If left Index is more than 0 keys away to the left, constrain
+                        self.current_finger_position[index] = self.current_finger_position[2]
+                elif index == 4:
+                    if self.current_finger_position[index] > self.current_finger_position[2]+9:
+                        # If left Thumb is more than 9 keys away to the right, constrain
+                        self.current_finger_position[index] = self.current_finger_position[2]+9
+                    elif self.current_finger_position[index] < self.current_finger_position[2]-4:
+                        # If left Thumb is more than 4 keys away to the left, constrain
+                        self.current_finger_position[index] = self.current_finger_position[2]-4
+                if self.fingers_count > 5:
+                    if index == 5:
+                        # Apply constrains Right hand fingers (constrained to right Middle)
+                        if self.current_finger_position[index] < self.current_finger_position[7]-9:
+                            # If right Thumb is more than 9 keys away to the left, constrain
+                            self.current_finger_position[index] = self.current_finger_position[7]-9
+                        elif self.current_finger_position[index] > self.current_finger_position[7]+4:
+                            # If right Thumb is more than 4 keys away to the right, constrain
+                            self.current_finger_position[index] = self.current_finger_position[7]+4
+                    elif index == 6:
+                        if self.current_finger_position[index] < self.current_finger_position[7]-5:
+                            # If right Index is more than 5 keys away to the left, constrain
+                            self.current_finger_position[index] = self.current_finger_position[7]-5
+                        elif self.current_finger_position[index] > self.current_finger_position[7]:
+                            # If right Index is more than 0 keys away to the right, constrain
+                            self.current_finger_position[index] = self.current_finger_position[7]
+                    elif index == 8: # We skip finger 7, since it is the Right Middle finger
+                        if self.current_finger_position[index] < self.current_finger_position[7]:
+                            # If right Ring is more than 0 keys away to the left, constrain
+                            self.current_finger_position[index] = self.current_finger_position[7]
+                        elif self.current_finger_position[index] > self.current_finger_position[7]+3:
+                            # If right Ring is more than 3 keys away to the right, constrain
+                            self.current_finger_position[index] = self.current_finger_position[7]+3
+                    elif index == 9:
+                        if self.current_finger_position[index] < self.current_finger_position[7]:
+                            # If right Pinky is more than 0 keys away to the left, constrain
+                            self.current_finger_position[index] = self.current_finger_position[7]
+                        elif self.current_finger_position[index] > self.current_finger_position[7]+5:
+                            # If right Pinky is more than 5 keys away to the right, constrain
+                            self.current_finger_position[index] = self.current_finger_position[7]+5
+
+
+        # One hot representation of keys with fingers on it
+        one_hot = [0 for _ in range(0, self.key_count*self.fingers_count)]
+        for index, finger_key in enumerate(self.current_finger_position):
+            if finger_key < 0 or finger_key > self.key_count:
+                continue # Skip if the finger is outside the keyboard
+            try:
+                one_hot[finger_key+(index*(self.key_count-1))] = 1
+            except IndexError:
+                print (finger_key)
+                print (index)
+                print (self.key_count)
+                print (finger_key+(index*(self.key_count-1)))
+                raise
+
+        if 0:
+            # TODO Not needed?
+            key_value = 0.0
+            if self.current_finger_position in self.frames[self.current_frame]:
+                if key_value == 1.0:
+                    reward = 0.5
+                else:
+                    reward = 0.01
             else:
                 reward = 0.01
-        else:
-            reward = 0.01
 
         done = False
         must_reset = False
+        reward = 0.0
 
         # If no frames left, done
         if self.current_frame >= len(self.frames)-1-1:
@@ -163,21 +290,25 @@ class PianoEnv:
         else:
             state = np.array(one_hot + self.frames[self.current_frame] + self.frames[self.current_frame + 1])
 
-
         if not must_reset:
-            # If the finger falls outside the keyboard, done
-            if self.current_finger_position < 0 or self.current_finger_position > self.key_count:
-                reward = 0.0001
-                must_reset = True
-
-        if not must_reset:
-            # If a key should be pressed but is not, done
-            for key, key_val in enumerate(self.frames[self.current_frame]):
-                if key_val == 1.0 and key != self.current_finger_position:
+            # If a finger falls outside the keyboard, done
+            for finger_number in range(0, self.fingers_count):
+                if self.current_finger_position[finger_number] < 0 or self.current_finger_position[finger_number] > self.key_count:
                     reward = 0.0001
+                    must_reset = True
+
+        if not must_reset:
+            for key, key_val in enumerate(self.frames[self.current_frame]):
+                if key_val == 1.0 and key not in self.current_finger_position:
+                    # If a key should be pressed but is not, increase failed_frames counter
+                    reward += 0.0001
                     self.failed_frames += 1
-                    if self.failed_frames > 30:
+                    if self.failed_frames > self.failed_frames_threshold:
+                        # If failed_frames counter > threshold, done
                         must_reset = True
+                elif key_val == 1.0 and key in self.current_finger_position:
+                    # If a key should be pressed and it is, reward
+                    reward += 0.05
 
         self.current_frame += 1
 
@@ -185,7 +316,7 @@ class PianoEnv:
             done = True
 
         info = {}
-        return state, reward, done, info
+        return state, reward+default_reward, done, info
 
 
 
@@ -194,7 +325,7 @@ class PianoEnv:
 
 # Create the Cart-Pole game environment
 #env = gym.make('CartPole-v0')
-env = PianoEnv('CartPole-v0', 'MIDI/test.midigram')
+env = PianoEnv('CartPole-v0', 'MIDI/Hummelflug.midigram')
 
 env.reset()
 rewards = []
@@ -223,7 +354,7 @@ class QNetwork:
 
             # Target Q values for training
             self.targetQs_ = tf.placeholder(tf.float32, [None], name='target')
-            self.keep_prob = tf.placeholder(tf.float32, [None], name='target')
+            self.keep_prob = tf.placeholder(tf.float32, [], name='keep_prob')
 
             # ReLU hidden layers
             self.fc1 = tf.contrib.layers.fully_connected(self.inputs_, hidden_size)
@@ -258,27 +389,31 @@ class Memory():
         return [self.buffer[ii] for ii in idx]
 
 
-train_episodes = 10000         # max number of episodes to learn from
+train_episodes = 20000         # max number of episodes to learn from
 max_steps = env.frame_count    # max steps in an episode
 gamma = 0.9                    # future reward discount
 
 # Exploration parameters
 explore_start = 0.99            # exploration probability at start
 explore_stop = 0.001            # minimum exploration probability
-decay_rate = 0.00001            # exponential decay rate for exploration prob
+decay_rate = 0.000001           # exponential decay rate for exploration prob
 
 # Network parameters
 hidden_size = 128              # number of units in each Q-network hidden layer
 learning_rate = 0.00001        # Q-network learning rate
 
 # Memory parameters
-memory_size = 100000           # memory capacity
-batch_size = 100               # experience mini-batch size
+memory_size = 1000000          # memory capacity
+batch_size = 128               # experience mini-batch size
 pretrain_length = batch_size   # number experiences to pretrain the memory
 
+do_training = False
 
 tf.reset_default_graph()
-mainQN = QNetwork(name='main', hidden_size=hidden_size, learning_rate=learning_rate, state_size=env.key_count*3, action_size=7)
+state_size = (env.key_count * 2) + (env.key_count * env.fingers_count)
+action_size = 5 ** env.fingers_count
+print ("State Size: {}, Action Size: {}".format(state_size, action_size))
+mainQN = QNetwork(name='main', hidden_size=hidden_size, learning_rate=learning_rate, state_size=state_size, action_size=action_size)
 
 # Initialize the simulation
 env.reset()
@@ -314,7 +449,6 @@ for ii in range(pretrain_length):
 saver = tf.train.Saver()
 
 loss = 0.0
-do_training = False
 if do_training:
     # Now train with experiences
     rewards_list = []
@@ -338,7 +472,7 @@ if do_training:
                     action = env.action_space.sample()
                 else:
                     # Get action from Q-network
-                    feed = {mainQN.inputs_: state.reshape((1, *state.shape)), mainQN.keep_prob=1.0}
+                    feed = {mainQN.inputs_: state.reshape((1, *state.shape)), mainQN.keep_prob: 1.0}
                     Qs = sess.run(mainQN.output, feed_dict=feed)
                     action = np.argmax(Qs)
                     #print (action)
@@ -352,11 +486,14 @@ if do_training:
                     # the episode ends so no next state
                     next_state = np.zeros(state.shape)
 
+                    if step % 100 == 0:
+                        saver.save(sess, "checkpoints/cartpole.ckpt")
+
                     if step % 10 == 0:
                         print('Episode: {}'.format(ep),
                               'Step: {:04d}'.format(t),
                               'Action: {:.4f}'.format(action),
-                              'Finger position: {:.4f}'.format(env.current_finger_position),
+                              #'Finger position: {:.4f}'.format(env.current_finger_position),
                               'Total reward: {:.4f}'.format(total_reward),
                               'Training loss: {:.4f}'.format(loss),
                               'Explore P: {:.4f}'.format(explore_p))
@@ -391,7 +528,7 @@ if do_training:
                 episode_ends = (next_states == np.zeros(states[0].shape)).all(axis=1)
                 #target_Qs[episode_ends] = (0, 0, 0, 0) # Actions
                 #target_Qs[episode_ends] = 0 # Actions
-                target_Qs[episode_ends] = [0 for _ in range(0,7)]
+                target_Qs[episode_ends] = [0 for _ in range(0,action_size)]
 
                 targets = rewards + gamma * np.max(target_Qs, axis=1)
 
@@ -439,7 +576,7 @@ with tf.Session() as sess:
             env.render()
 
             # Get action from Q-network
-            feed = {mainQN.inputs_: state.reshape((1, *state.shape)), mainQN.keep_prob=0.7}
+            feed = {mainQN.inputs_: state.reshape((1, *state.shape)), mainQN.keep_prob: 0.7}
             Qs = sess.run(mainQN.output, feed_dict=feed)
             action = np.argmax(Qs)
 
