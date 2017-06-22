45c3aa70df48b57095f7e2ee908e15af8e4ed6f0
Eibriel <eibriel@yahoo.com.ar>
2017-06-22 18:55:27 -0300

    Adding config sample file

---
+++ music/config_sample.py
@@ -0,0 +1,10 @@
+class Config:
+    midigram_filepath = 'MIDI/Nyan_Cat_Piano.midigram'
+    octave_shift = -2
+    frame_steps = 10 # Window
+    max_steps = 100 # Quality
+    sufix = 0
+    specimens_count = 400
+    percent_to_clone = 0.5
+    percent_to_select = 0.2
+    mutation_probability = 0.5
